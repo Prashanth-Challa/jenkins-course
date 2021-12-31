a6b5d4bce0e68cf949d6c3cb61fe292aa23add73
Prashanth-Challa <63869971+Prashanth-Challa@users.noreply.github.com>
2021-12-31 18:18:54 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/Prashanth-Challa/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/Prashanth-Challa/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
